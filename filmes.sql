28723d900923385ecb0396b85453073ef12ee5c2
lucas <lucasmelo8328@gmail.com>
2024-03-05 15:27:25 -0300

    nada, sério...

---
+++ filmes.sql
@@ -0,0 +1,73 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1deb5ubuntu1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Tempo de geração: 05/03/2024 às 14:51
+-- Versão do servidor: 8.0.35-0ubuntu0.22.04.1
+-- Versão do PHP: 8.1.2-1ubuntu2.14
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `Filmes`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `filmes`
+--
+
+CREATE TABLE `filmes` (
+  `id` int NOT NULL,
+  `ano` int NOT NULL,
+  `paisOrigem` varchar(20) NOT NULL,
+  `sinopse` varchar(255) NOT NULL,
+  `id_produtora` int NOT NULL,
+  `nome` varchar(40) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices de tabela `filmes`
+--
+ALTER TABLE `filmes`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_produtora` (`id_produtora`);
+
+--
+-- AUTO_INCREMENT para tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `filmes`
+--
+ALTER TABLE `filmes`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- Restrições para tabelas despejadas
+--
+
+--
+-- Restrições para tabelas `filmes`
+--
+ALTER TABLE `filmes`
+  ADD CONSTRAINT `fk_produtora` FOREIGN KEY (`id_produtora`) REFERENCES `produtoras` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
